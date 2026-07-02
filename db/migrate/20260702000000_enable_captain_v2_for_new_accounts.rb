787c1a6c3a945b7901d0f215276102601805e5d1
aakashb95 <aakashbakhle@gmail.com>
2026-07-02 14:34:47 +0530

    fix: cleanup defensive checks

--- db/migrate/20260702000000_enable_captain_v2_for_new_accounts.rb
+++ db/migrate/20260702000000_enable_captain_v2_for_new_accounts.rb
@@ -1,13 +1,12 @@
 class EnableCaptainV2ForNewAccounts < ActiveRecord::Migration[7.1]
   def up
-    config = InstallationConfig.find_by(name: 'ACCOUNT_LEVEL_FEATURE_DEFAULTS')
-    return if config&.value.blank?
-
+    config = InstallationConfig.find_by!(name: 'ACCOUNT_LEVEL_FEATURE_DEFAULTS')
     features = config.value
-    captain_features = features.select { |f| %w[captain_integration captain_integration_v2].include?(f['name']) }
-    return if captain_features.blank?
 
-    captain_features.each { |feature| feature['enabled'] = true }
+    %w[captain_integration captain_integration_v2].each do |feature_name|
+      features.find { |feature| feature['name'] == feature_name }['enabled'] = true
+    end
+
     config.update!(value: features)
     GlobalConfig.clear_cache
   end
